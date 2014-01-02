e7094c1f54f9ef2afcfa498d04e85fee90710bca
Gaby <jgabrielcorpas@icloud.com>
2014-01-02 13:16:28 +0100

    Customizable timeout

--- notifIt!-1.1/js/notifIt.js
+++ notifIt!-1.1/js/notifIt.js
@@ -3,18 +3,16 @@
 var to, width, height, position, autohide, opacity, time;
 
 function notifit_setDefaultValues(){
-    // Default size
     width = 400;
     height = 60;
-    // Default position
     position = "right";
-    // Default autohide
     autohide = true;
-	// Default msg
 	msg = "";
-	// Default opacity (Only Chrome, Firefox and Safari)
-	opacity = 1;
-	// Default time
+	opacity = 1; // Default opacity (Only Chrome, Firefox and Safari)
+	multiline = false;
+	fade = false;
+	bgcolor = "#444";
+	color = "#EEE";
 	time = 5000;
 }
 function notif(config){
@@ -35,6 +33,8 @@ function notif(config){
 			width = screen.width - 60; // Margin difference
 		}
     }
+	if(config.fade){ fade = config.fade; }
+	if(config.multiline){ multiline = config.multiline; }
     if(config.height){
         if(config.height < 100 && config.height > 0){
             height = config.height; // Take the height in pixels
@@ -43,13 +43,16 @@ function notif(config){
 	if(typeof config.autohide !== "undefined")
 	    autohide = config.autohide;
     var div = "<div id='ui_notifIt'><p>"+((config.msg) ? config.msg : "")+"</p></div>";
-    
-    $("#ui_notifIt").remove(); // Preventive remove
+    $("#ui_notifIt").remove();// Preventive remove
     clearInterval(to); // Preventive clearInterval
     $("body").append(div);
-    $("#ui_notifIt").css("height", height);
+	if(multiline){
+		$("#ui_notifIt").css("padding", 15);
+	}else{
+		$("#ui_notifIt").css("height", height);
+	    $("#ui_notifIt p").css("line-height", height+"px");
+	}
     $("#ui_notifIt").css("width", width);
-
     switch(position){
         case "center":
             $("#ui_notifIt").css("top", parseInt(0-(height+10)));
@@ -64,9 +67,7 @@ function notif(config){
             $("#ui_notifIt").css("right", parseInt(0-(width+10)));
             break;
     }
-
 	if(config.opacity){ $("#ui_notifIt").css("opacity", config.opacity); }
-    
     switch(config.type){
         case "error":
             $("#ui_notifIt").addClass("error");
@@ -84,7 +85,13 @@ function notif(config){
             $("#ui_notifIt").addClass("default");
             break;
     }
-    
+	// Override color if given
+	if(config.bgcolor){
+		$("#ui_notifIt").css("background-color", config.bgcolor);
+	}
+	if(config.color){
+		$("#ui_notifIt").css("color", config.color);
+	}
     switch(position){
         case "left":
             $("#ui_notifIt").css("left", parseInt(0-(width*2)));
@@ -101,8 +108,6 @@ function notif(config){
             $("#ui_notifIt").css("left", mid-parseInt(width/2));
             break;
     }
-    $("#ui_notifIt p").css("line-height", height+"px");
-
     switch(position){
         case "center":
             $("#ui_notifIt").animate({top: 10});
@@ -117,55 +122,57 @@ function notif(config){
             $("#ui_notifIt").animate({right: 10});
             break;
     }
-    
-
     $("#ui_notifIt").click(function(){
         notifit_dismiss();
     });
-
-    if(autohide == true){
-    	if(config.time){
-    		if(!isNaN(config.time)){
-    			time = config.time;
+    if(autohide){
+    	if(config.timeout){
+    		if(!isNaN(config.timeout)){
+    			time = config.timeout;
     		}
     	}
-        to = setTimeout(function(){notifit_dismiss();}, time);
-    }
-    
+        to = setTimeout(function(){notifit_dismiss();},time);
+	}
 }
 
 function notifit_dismiss(){
     clearInterval(to);
-    if(position == "center"){
-        $("#ui_notifIt").animate({
-            top: parseInt(height-(height/2))
-        }, 100, function(){
-            $("#ui_notifIt").animate({
-                top: parseInt(0-(height*2))
-            }, 100, function(){
-                $("#ui_notifIt").remove();
-                });
-        });
-    }else if(position == "right"){
-        $("#ui_notifIt").animate({
-            right: parseFloat(width-(width*0.9))
-        }, 100, function(){
-            $("#ui_notifIt").animate({
-                right: parseInt(0-(width*2))
-            }, 100, function(){
-                $("#ui_notifIt").remove();
-                });
-        });
-    }else if(position == "left"){
-        $("#ui_notifIt").animate({
-            left: parseFloat(width-(width*0.9))
-        }, 100, function(){
-            $("#ui_notifIt").animate({
-                left: parseInt(0-(width*2))
-            }, 100, function(){
-                $("#ui_notifIt").remove();
-                });
-        });
-    }
+	if(!fade){
+		if(position == "center"){
+		    $("#ui_notifIt").animate({
+		        top: parseInt(height-(height/2))
+		    }, 100, function(){
+		        $("#ui_notifIt").animate({
+		            top: parseInt(0-(height*2))
+		        }, 100, function(){
+		            $("#ui_notifIt").remove();
+		            });
+		    });
+		}else if(position == "right"){
+		    $("#ui_notifIt").animate({
+		        right: parseFloat(width-(width*0.9))
+		    }, 100, function(){
+		        $("#ui_notifIt").animate({
+		            right: parseInt(0-(width*2))
+		        }, 100, function(){
+		            $("#ui_notifIt").remove();
+		            });
+		    });
+		}else if(position == "left"){
+		    $("#ui_notifIt").animate({
+		        left: parseFloat(width-(width*0.9))
+		    }, 100, function(){
+		        $("#ui_notifIt").animate({
+		            left: parseInt(0-(width*2))
+		        }, 100, function(){
+		            $("#ui_notifIt").remove();
+		            });
+		    });
+		}
+	}else{
+		$("#ui_notifIt").fadeOut("slow", function(){
+			$("#ui_notifIt").remove();
+		});
+	}
     notifit_setDefaultValues();
 }
