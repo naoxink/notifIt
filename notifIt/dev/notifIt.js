996098409019997f55ace94f86d37a1f1c56183b
J. Gabriel Corpas <naoxink@icloud.com>
2015-06-17 12:54:50 +0200

    Refactored
    Begun to restructure the code for better reading and management

---
+++ notifIt/dev/notifIt.js
@@ -0,0 +1,250 @@
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else {
+        // Browser globals
+        var package = factory(root.b);
+        root.notif = package.notif;
+        root.notifit_dismiss = package.notifit_dismiss;
+    }
+}(this, function() {
+    function notif(config) {
+        // Vars
+        var to = null;
+        var height = 0;
+        var options = {};
+        var defaults = {};
+        var $ = jQuery;
+        
+        // METHODS
+        this._init = function() {
+            defaults = {
+                type: "info",
+                width: 400,
+                height: 60,
+                position: "right",
+                autohide: 1,
+                msg: "This is my default message",
+                opacity: 1,
+                multiline: 0,
+                fade: 0,
+                bgcolor: "",
+                color: "",
+                timeout: 5000,
+                zindex: null,
+                offset: 0,
+                callback: null,
+                clickable: false
+            };
+        }
+        this._setConfig = function(config) {
+            $.extend(defaults, config);
+            if (typeof defaults.callback !== 'function') {
+                defaults.callback = null;
+            }
+            this._setWidth();
+            this._setHeight();
+        }
+        this._setWidth = function() {
+            if (defaults.width > 0) {
+                defaults.width = defaults.width;
+            } else if (defaults.width === "all") {
+                defaults.width = screen.width - 60;
+            }
+        }
+        this._setHeight = function() {
+            if (defaults.height > 100 || defaults.height < 0) {
+                defaults.height = defaults.height;
+            }
+        }
+        this._build = function() {
+            var div = $('<div>', {
+                id: 'ui_notifIt'
+            });
+            var p = $('<p>', {
+                html: defaults.msg
+            });
+            div.append(p);
+            this._remove();
+            this._append(div);
+        }
+        this._append = function(div){
+            if(!defaults.container){
+                $('body').append(div);
+            }else{
+                $(defaults.container).append(div);
+            }
+        }
+        this._remove = function() {
+            $("#ui_notifIt").remove();
+            clearInterval(to);
+        }
+        this._create = function() {
+            $("body").append(div);
+            _setCss();
+        }
+        this._setCss = function() {
+            if (defaults.zindex) {
+                $("#ui_notifIt").css("z-index", defaults.zindex);
+            }
+            if (defaults.multiline) {
+                $("#ui_notifIt").css("padding", 15);
+            } else {
+                $("#ui_notifIt").css("height", defaults.height);
+                $("#ui_notifIt p").css("line-height", defaults.height + "px");
+            }
+            switch (defaults.type) {
+                case "error":
+                    $("#ui_notifIt").addClass("error");
+                    break;
+                case "success":
+                    $("#ui_notifIt").addClass("success");
+                    break;
+                case "info":
+                    $("#ui_notifIt").addClass("info");
+                    break;
+                case "warning":
+                    $("#ui_notifIt").addClass("warning");
+                    break;
+                default:
+                    $("#ui_notifIt").addClass("default");
+                    break;
+            }
+            $("#ui_notifIt").css("background-color", defaults.bgcolor);
+            $("#ui_notifIt").css("color", defaults.color);
+            $("#ui_notifIt").css("width", defaults.width);
+            $("#ui_notifIt").css("opacity", defaults.opacity);
+            switch (defaults.position) {
+                case "left":
+                    $("#ui_notifIt").css("left", parseInt(0 - (defaults.width + 10)));
+                    $("#ui_notifIt").css("left", parseInt(0 - (defaults.width * 2)));
+                    $("#ui_notifIt").animate({
+                        left: parseInt(10 + defaults.offset)
+                    });
+                    break;
+                case "right":
+                    $("#ui_notifIt").css("right", parseInt(0 - (defaults.width + 10)));
+                    $("#ui_notifIt").css("right", parseInt(0 - (defaults.width * 2)));
+                    $("#ui_notifIt").animate({
+                        right: parseInt(10 + defaults.offset)
+                    });
+                    break;
+                case "center":
+                    var mid = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) / 2;
+                    $("#ui_notifIt").css("top", parseInt(0 - (defaults.height + 10)));
+                    $("#ui_notifIt").css("left", mid - parseInt(defaults.width / 2));
+                    $("#ui_notifIt").animate({
+                        top: parseInt(10 + defaults.offset)
+                    });
+                    break;
+                case "bottom":
+                    var mid = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) / 2;
+                    $("#ui_notifIt").css("top", 'auto');
+                    $("#ui_notifIt").css("bottom", parseInt(0 - (defaults.height + 10)));
+                    $("#ui_notifIt").css("left", mid - parseInt(defaults.width / 2));
+                    $("#ui_notifIt").animate({
+                        bottom: parseInt(10 + defaults.offset)
+                    });
+                    break;
+                default:
+                    var mid = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) / 2;
+                    $("#ui_notifIt").css("right", parseInt(0 - (defaults.width + 10)));
+                    $("#ui_notifIt").css("left", mid - parseInt(defaults.width / 2));
+                    $("#ui_notifIt").animate({
+                        right: parseInt(10 + defaults.offset)
+                    });
+                    break;
+            }
+        }
+
+        this._setEvents = function(){
+            if (!defaults.clickable) {
+                $("#ui_notifIt").click(function(e) {
+                    e.stopPropagation();
+                    notifit_dismiss(to, defaults);
+                });
+            }
+            $('body').on('click', '#ui_notifIt #notifIt_close', function() {
+                notifit_dismiss(to, defaults);
+            });
+            if (defaults.autohide) {
+                if (!isNaN(defaults.timeout)) { // Take the timeout if is a number
+                    to = setTimeout(function() {
+                        notifit_dismiss(to, defaults);
+                    }, defaults.timeout);
+                }
+            }
+        }
+
+
+        this._init();
+        this._setConfig(config);
+        this._build();
+        this._setCss();
+        this._setEvents();        
+
+    }
+
+    function notifit_dismiss(to, config) {
+        clearInterval(to);
+        if (!config.fade) {
+            var animation1 = {},
+                animation2 = {};
+            switch (config.position) {
+                case "center":
+                    animation1 = {
+                        top: parseInt(config.height - (config.height / 2))
+                    };
+                    animation2 = {
+                        top: parseInt(0 - (config.height * 2))
+                    };
+                    break;
+                case "bottom":
+                    animation1 = {
+                        bottom: parseInt(config.height - (config.height / 2))
+                    };
+                    animation2 = {
+                        bottom: parseInt(0 - (config.height * 2))
+                    };
+                    break;
+                case "right":
+                    animation1 = {
+                        right: parseFloat(config.width - (config.width * 0.9))
+                    };
+                    animation2 = {
+                        right: parseInt(0 - (config.width * 2))
+                    };
+                    break;
+                case "left":
+                    animation1 = {
+                        left: parseFloat(config.width - (config.width * 0.9))
+                    };
+                    animation2 = {
+                        left: parseInt(0 - (config.width * 2))
+                    };
+                    break;
+            }
+            // Execute animations       
+            $("#ui_notifIt").animate(animation1, 100, function() {
+                $("#ui_notifIt").animate(animation2, 100, function() {
+                    $("#ui_notifIt").remove();
+                    if (config.callback) {
+                        return config.callback();
+                    }
+                });
+            });
+        } else {
+            $("#ui_notifIt").fadeOut("slow", function() {
+                $("#ui_notifIt").remove();
+                if (config.callback) {
+                    return config.callback();
+                }
+            });
+        }
+    }
+    return {
+        notif: notif,
+        notifit_dismiss: notifit_dismiss
+    };
+}));
