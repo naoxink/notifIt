10f193dfd381338fe81c87ee3d8b9d0885a9f797
J. Gabriel Corpas <naoxink@icloud.com>
2015-09-01 08:08:43 +0200

    Animation support, switches removed & brand new comments

--- notifIt/js/notifIt.js
+++ notifIt/js/notifIt.js
@@ -16,7 +16,7 @@
 function notif(config) {
     var to = null;
     var defaults = {
-        type: "info",
+        type: "default",
         width: 400,
         height: 60,
         position: "right",
@@ -30,177 +30,215 @@ function notif(config) {
         timeout: 5000,
         zindex: null,
         offset: 0,
-		callback: null,
-		clickable: false
+	callback: null,
+	clickable: false,
+        animation: 'slide'
     };
-    jQuery.extend(defaults, config);
+    var $ = jQuery;
 
-	if(typeof defaults.callback !== 'function'){
-		defaults.callback = null;
-	}
-    
-    position = defaults.position;
+    $.extend(defaults, config);
 
-    if (defaults.width > 0) {
-        defaults.width = defaults.width;
-    } else if (defaults.width === "all") {
-        defaults.width = screen.width - 60;
+    // Mid position
+    var mid = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) / 2;
+
+    defaults.animations = {
+        'slide': {
+            'center': {
+                'css_start': {
+                    "top": parseInt(0 - (defaults.height + 10)),
+                    "left": mid - parseInt(defaults.width / 2)
+                },
+                'in': { 'top': parseInt(10 + defaults.offset) },
+                'out': {
+                    'start': { 'top': parseInt(defaults.height - (defaults.height / 2)) },
+                    'end': { 'top': parseInt(0 - (defaults.height * 2)) }
+                }
+            },
+            'bottom': {
+                'css_start': { 
+                    "top": 'auto', 
+                    "bottom": parseInt(0 - (defaults.height + 10)), 
+                    "left": mid - parseInt(defaults.width / 2)
+                },
+                'in': { 'bottom': parseInt(10 + defaults.offset) },
+                'out': {
+                    'start': { 'top': parseInt(defaults.height - (defaults.height / 2)) },
+                    'end': { 'bottom': parseInt(0 - (defaults.height * 2)) }
+                }
+            },
+            'right': {
+                'css_start': {
+                    "right": parseInt(0 - (defaults.width + 10)),
+                    "right": parseInt(0 - (defaults.width * 2))
+                },
+                'in': { 'right': parseInt(10 + defaults.offset) },
+                'out':{
+                    'start': { 'right': parseFloat(defaults.width - (defaults.width * 0.9)) },
+                    'end': { 'right': parseInt(0 - (defaults.width * 2)) }
+                }
+            },
+            'left': {
+                'css_start': {
+                    "left": parseInt(0 - (defaults.width + 10))
+                },
+                'in': { 'left': parseInt(10 + defaults.offset) },
+                'out': {
+                    'start': { 'left': parseFloat(defaults.width - (defaults.width * 0.9)) },
+                    'end': { 'left': parseInt(0 - (defaults.width * 2)) }
+                }
+            }
+        },
+        'zoom': {
+            'right': {
+                'in': { 'right': parseInt(10 + defaults.offset) , 'zoom': 1 },
+                'out': {
+                    'start': { 'zoom': 1.3 },
+                    'end': { 'zoom': 0.01 }
+                }
+            },
+            'left': {
+                'in': { 'left': parseInt(10 + defaults.offset) , 'zoom': 1 },
+                'out': {
+                    'start': { 'zoom': 1.3 },
+                    'end': { 'zoom': 0.01 }
+                }
+            }
+        }
+    };
+
+    // Check if animation exists
+    defaults.available_animations = Object.keys(defaults.animations)
+    if(!defaults.available_animations.length){
+        throw new Error('No animations')
+    }
+    if(defaults.available_animations.indexOf(defaults.animation) === -1){
+        defaults.animation = defaults.available_animations[0]
+    }
+    // Check callback
+    if(typeof defaults.callback !== 'function'){
+	defaults.callback = null;
     }
 
+    // Width & Height
+    if (defaults.width > 0) {
+        defaults.width = defaults.width;
+    }else if(defaults.width === "all"){
+        defaults.width = screen.width - 60;
+    }else{
+        defaults.width = 400;
+    }
     if (defaults.height < 100 && defaults.height > 0) {
         height = defaults.height;
     }
 
-    var div = "<div id='ui_notifIt'><p>" + defaults.msg + "</p></div>";
-    jQuery("#ui_notifIt").remove();
+    // Create notification itself
+    var div = $('<div>', {
+        'id': 'ui_notifIt'
+    });
+    var p = $('<p>', {
+        html: defaults.msg
+    });
+    div.append(p);
+
+    // Remove div before appending, we don't want to duplicate
+    $("#ui_notifIt").remove();
     clearInterval(to);
-    jQuery("body").append(div);
+    $("body").append(div);
 
-    if ( defaults.zindex ) {
-        jQuery("#ui_notifIt").css("z-index", defaults.zindex);
+    if(defaults.zindex){
+        $("#ui_notifIt").css("z-index", defaults.zindex);
     }
 
+    // If multiline we have to set the padding instead line-height
     if (defaults.multiline) {
-        jQuery("#ui_notifIt").css("padding", 15);
+        $("#ui_notifIt").css("padding", 15);
     } else {
-        jQuery("#ui_notifIt").css("height", height);
-        jQuery("#ui_notifIt p").css("line-height", height + "px");
+        $("#ui_notifIt").css("height", height);
+        $("#ui_notifIt p").css("line-height", height + "px");
     }
 
-    jQuery("#ui_notifIt").css("width", defaults.width);
+    // Basic css
+    $("#ui_notifIt").css({
+        "width": defaults.width,
+        "opacity": defaults.opacity,
+        "background-color": defaults.bgcolor,
+        "color": defaults.color
+    });
 
-    jQuery("#ui_notifIt").css("opacity", defaults.opacity);
-
-    switch (defaults.type) {
-        case "error":
-            jQuery("#ui_notifIt").addClass("error");
-            break;
-        case "success":
-            jQuery("#ui_notifIt").addClass("success");
-            break;
-        case "info":
-            jQuery("#ui_notifIt").addClass("info");
-            break;
-        case "warning":
-            jQuery("#ui_notifIt").addClass("warning");
-            break;
-        default:
-            jQuery("#ui_notifIt").addClass("default");
-            break;
+    // Class 'success', 'error', 'warning', 'info'..
+    $("#ui_notifIt").addClass(defaults.type);
+     
+    // Set entry animation   
+    if(defaults.animations[defaults.animation][defaults.position].css_start){
+        $("#ui_notifIt").css(defaults.animations[defaults.animation][defaults.position].css_start);
+    }else{
+        $("#ui_notifIt").css(defaults.animations[defaults.available_animations[0]][defaults.position].css_start);
     }
-
-    jQuery("#ui_notifIt").css("background-color", defaults.bgcolor);
+    // Execute entry animation
+    $("#ui_notifIt").animate(defaults.animations[defaults.animation][defaults.position].in);
     
-    jQuery("#ui_notifIt").css("color", defaults.color);
-    
-    switch (defaults.position) {
-        case "left":
-            jQuery("#ui_notifIt").css("left", parseInt(0 - (defaults.width + 10)));
-            jQuery("#ui_notifIt").css("left", parseInt(0 - (defaults.width * 2)));
-            jQuery("#ui_notifIt").animate({left: parseInt(10 + defaults.offset)});
-            break;
-        case "right":
-            jQuery("#ui_notifIt").css("right", parseInt(0 - (defaults.width + 10)));
-            jQuery("#ui_notifIt").css("right", parseInt(0 - (defaults.width * 2)));
-            jQuery("#ui_notifIt").animate({right: parseInt(10 + defaults.offset)});
-            break;
-        case "center":
-            var mid = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) / 2;
-            jQuery("#ui_notifIt").css("top", parseInt(0 - (defaults.height + 10)));
-            jQuery("#ui_notifIt").css("left", mid - parseInt(defaults.width / 2));
-            jQuery("#ui_notifIt").animate({top: parseInt(10 + defaults.offset)});
-            break;
-        case "bottom":
-            var mid = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) / 2;
-            jQuery("#ui_notifIt").css("top", 'auto');
-            jQuery("#ui_notifIt").css("bottom", parseInt(0 - (defaults.height + 10)));
-            jQuery("#ui_notifIt").css("left", mid - parseInt(defaults.width / 2));
-            jQuery("#ui_notifIt").animate({bottom: parseInt(10 + defaults.offset)});
-            break;
-        default:
-            var mid = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) / 2;
-            jQuery("#ui_notifIt").css("right", parseInt(0 - (defaults.width + 10)));
-            jQuery("#ui_notifIt").css("left", mid - parseInt(defaults.width / 2));
-            jQuery("#ui_notifIt").animate({right: parseInt(10 + defaults.offset)});
-            break;
+    // Events
+    if(!defaults.clickable){
+        $("#ui_notifIt").click(function(e) {
+            e.stopPropagation();
+            notifit_dismiss(to, defaults);
+        });
     }
-    
-	if(!defaults.clickable){
-		jQuery("#ui_notifIt").click(function(e) {
-		    e.stopPropagation();
-		    notifit_dismiss(to, defaults);
-		});
-	}
-
-	jQuery('body').on('click', '#ui_notifIt #notifIt_close', function() {
-	    notifit_dismiss(to, defaults);
-	});
+    $('body').on('click', '#ui_notifIt #notifIt_close', function() {
+        notifit_dismiss(to, defaults);
+    });
 
     if (defaults.autohide) {
-            if (!isNaN(defaults.timeout)) { // Take the timeout if is a number
-                to = setTimeout(function() {
-                   notifit_dismiss(to, defaults);
-                }, defaults.timeout);
-            }
-        
+        if (!isNaN(defaults.timeout)) {
+            to = setTimeout(function() {
+               notifit_dismiss(to, defaults);
+            }, defaults.timeout);
+        }
     }
 }
 
 function notifit_dismiss(to, config) {
     clearTimeout(to);
 
-    if (!config.fade) {
-		var animation1 = {},
-			animation2 = {};
-        switch(config.position){
-            case "center":
-				animation1 = {
-                    top: parseInt(config.height - (config.height / 2))
-                };
-				animation2 = {
-                    top: parseInt(0 - (config.height * 2))
-                };
-            break;
-            case "bottom":
-				animation1 = {
-                    bottom: parseInt(config.height - (config.height / 2))
-                };
-				animation2 = {
-                    bottom: parseInt(0 - (config.height * 2))
-                };
-            break;
-            case "right":
-				animation1 = {
-                    right: parseFloat(config.width - (config.width * 0.9))
-                };
-				animation2 = {
-                    right: parseInt(0 - (config.width * 2))
-                };
-            break;
-            case "left":
-				animation1 = {
-                    left: parseFloat(config.width - (config.width * 0.9))
-                };
-				animation2 = {
-                    left: parseInt(0 - (config.width * 2))
-                };
-            break;
+    if (config.animation != 'fade'){
+        // Set animations
+        if(
+            config.animations && 
+            config.animations[config.animation] && 
+            config.animations[config.animation][config.position] &&
+            config.animations[config.animation][config.position].out &&
+            config.animations[config.animation][config.position].out.start &&
+            config.animations[config.animation][config.position].out.end){
+            animation1 = config.animations[config.animation][config.position].out.start
+            animation2 = config.animations[config.animation][config.position].out.end
+        }else if(
+            config.animations[config.available_animations[0]] &&
+            config.animations[config.available_animations[0]][config.position] &&
+            config.animations[config.available_animations[0]][config.position].out &&
+            config.animations[config.available_animations[0]][config.position].out.start &&
+            config.animations[config.available_animations[0]][config.position].out.end){
+            animation1 = config.animations[config.available_animations[0]][config.position].out.start
+            animation2 = config.animations[config.available_animations[0]][config.position].out.end
+        }else{
+            throw new Error('Invalid animation')
         }
-		// Execute animations		
-        jQuery("#ui_notifIt").animate(animation1, 100, function() {
-            jQuery("#ui_notifIt").animate(animation2, 100, function() {
-                jQuery("#ui_notifIt").remove();
-				if(config.callback){
-					config.callback();
-				}
+
+	// Execute animations		
+        $("#ui_notifIt").animate(animation1, 100, function() {
+            $("#ui_notifIt").animate(animation2, 100, function() {
+                $("#ui_notifIt").remove();
+		if(config.callback){
+		    config.callback();
+		}
             });
         });
     } else {
-        jQuery("#ui_notifIt").fadeOut("slow", function() {
-            jQuery("#ui_notifIt").remove();
-			if(config.callback){
-				config.callback();
-			}
+    	// jQuery's fade, why create my own?
+        $("#ui_notifIt").fadeOut("slow", function() {
+            $("#ui_notifIt").remove();
+	    if(config.callback){
+	        config.callback();
+	    }
         });
     }
 }
